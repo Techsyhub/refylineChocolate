cb586b50315cfed6f7887bc1ec97bd25d14b0f78
techsydev <techsyhub@gmail.com>
2025-12-25 05:05:06 +0500

    update image protocol

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ['res.cloudinary.com'], // add your Cloudinary domain here
+     remotePatterns: [
+    { protocol: 'https', hostname: '**' }
+  ]
   },
 };
 
